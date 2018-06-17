e8d1d5e2495902426b7e6337edae215c1c0190f9
hs <>
2018-06-17 14:10:14 +0200

    fixed testing

--- src/view/MainExample.spec.ts
+++ src/view/MainExample.spec.ts
@@ -1,13 +1,21 @@
-const hslayout = require('hslayout');
-const m = hslayout.m;
-const o = hslayout.o;
+if (!global['window']) {
+    console.log('creating non-browser polyfill');
+    // Polyfill DOM env for mithril
+    global['window'] = require("mithril/test-utils/browserMock.js")();
+    global['document'] = window.document;
 
+}
+
+const o = require("mithril/ospec/ospec");
+const m = require("mithril");
+
+const root = window.document.createElement("div");
 
 o.spec('example', () => {
     let docsMenu:any;
     o.before(() => {
-        m.mount(o.root, {view: () => m('', '')});
-        docsMenu = o.root.childNodes[0];
+        m.mount(root, {view: () => m('', '')});
+        docsMenu = root.childNodes[0];
     }); 
     o('DocsMenu exists', () => {
         o(docsMenu===undefined).equals(false)('should be defined');
