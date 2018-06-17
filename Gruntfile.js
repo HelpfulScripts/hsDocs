7274b6baf7696f968925ecc551ee818a44b92e9f
hs <>
2018-06-17 13:10:38 +0200

    initial version

--- Gruntfile.js
+++
@@ -1,5 +0,0 @@
-/*global module:false*/
-module.exports = function(grunt) {
-    const sgc  = require('../../sharedGruntConfig')(grunt, __dirname, ['hsLayout', 'hsWidget', 'hsGraph', 'hsdatab', 'hsUtil'], 'app');
-	grunt.initConfig(sgc); 
-};
