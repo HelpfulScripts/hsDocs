ea6099bf0ceedec9c62726bbd0ca6b11ec8baf27
hs <>
2018-07-04 05:48:00 +0200

    fixed path to sharedGruntConfig

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /*global module:false*/
 module.exports = function(grunt) {
-    const sgc  = require('../../sharedGruntConfig')(grunt, __dirname, ['hsLayout', 'hsWidget', 'hsGraph', 'hsdatab', 'hsUtil'], 'app');
+    const sgc  = require('./sharedGruntConfig')(grunt, __dirname, ['hsLayout', 'hsWidget', 'hsGraph', 'hsdatab', 'hsUtil'], 'app');
 	grunt.initConfig(sgc); 
 };
