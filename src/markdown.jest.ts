cbcd2d05ca3952dd6a5565812c94d8cda9db63e4
hs <helpfulscripts@gmail.com>
2018-11-16 20:24:46 -0800

    added markdown testing

--- src/markdown.jest.ts
+++ src/markdown.jest.ts
@@ -1,8 +1,15 @@
 import { markDown } from './markdown';
+import { m } from 'hslayout';
+
 
 describe('markdown', () => {
     test('markDown long', ()=>{
         expect(markDown('*bold*', false, '#')).toEqual('<p><em>bold</em></p>');
+    });
+    
+    test('markDown default long', ()=>{
+        m.route.get = () => '#';
+        expect(markDown('*bold*')).toEqual('<p><em>bold</em></p>');
     });
     
     test('markDown short', ()=>{
@@ -12,5 +19,6 @@ describe('markdown', () => {
     test('links', () => {
         expect(markDown('a {@link hsDocs/one two}', true, '#')).toEqual('<p>a <a href=\"#!/api/undefined/undefined.hsDocs/one\">two</a>');
         expect(markDown('a {@link hsDocs:one two}', true, '#')).toEqual('<p>a <a href=\"#!/api/hsDocs/hsDocs.one\">two</a>');
+        expect(markDown('a {@link hsDocs#one two}', true, '#')).toEqual('<p>a <a href=\"#!/api/undefined/undefined.hsDocs\">two</a>');
     });
 });
