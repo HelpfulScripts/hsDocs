dd6f11ebd0722cbd5d3199bd5ae4699dce9be90b
hs <helpfulscripts@gmail.com>
2019-09-02 14:24:59 -0700

    added direct page layout

--- src/Site.ts
+++ src/Site.ts
@@ -8,8 +8,9 @@ import * as header  from './view/DocsMenu';
 import * as left    from './view/LeftNav';
 import * as main    from './view/MainDetail';
 
-import {m, Vnode}   from 'hslayout';
-import { Config }   from 'hslayout';
+// import { Layout }   from 'hslayout';
+// import { m } from 'hslayout';
+// import { Config }   from 'hslayout';
 
 const TitleHeight   = '30px'; 
 const FooterHeight  = '10px';  
@@ -46,14 +47,72 @@ const myConfig2 = {
     mainDetail: {MainDetail: {}}
 }; 
 
+const docWindow = {
+    view: () => hslayout.m(hslayout.Layout, {
+        rows:  [TitleHeight, "fill", FooterHeight],
+        css: '.hs-site-main',
+        content: [
+            hslayout.m(docHeader, {}),
+            hslayout.m(docBody, {}),
+            hslayout.m(docFooter, {})
+        ] 
+    })
+};
+
+const docHeader = {
+    view: () => hslayout.m(hslayout.Layout, { 
+        columns: [LeftNavWidth, "fill"],
+        css: '.hs-site-header',
+        content: [
+            hslayout.m(title, {}),
+            hslayout.m(menu, {})
+        ]                
+    })
+};
+
+const docBody = {
+    view: () => hslayout.m(hslayout.Layout, { 
+        columns: [LeftNavWidth, "fill"], 
+        content: [
+            hslayout.m(left.LeftNav, {}), 
+            hslayout.m(main.MainDetail, {})
+        ]                
+    })
+};
+
+const docFooter = {
+    view: () => hslayout.m(hslayout.Layout, {
+        css: '.hs-site-footer',
+        content: '(c) Helpful Scripts'
+    })
+};
+
+const title = {
+    view: () => hslayout.m(hslayout.Layout, { 
+        css: '.hs-site-title',
+        content: 'HSDocs', 
+        href: 'https://github.com/HelpfulScripts/hsDocs' 
+    })
+};
+
+const menu = {
+    view: () => hslayout.m(header.DocsMenu, { 
+        docSet:"./data/index.json"
+    })
+};
+
+const useConfig = false;
 
 export function init() {
     const Router = {
-        view: (node:Vnode) =>
-            m(Config, Object.assign({source: myConfig2, context: [hslayout, header, left, main]}, 
-                                    {route: {field:m.route.param('field'), lib:m.route.param('lib')}}))
+        view: () => {
+            const param = {route: {field:hslayout.m.route.param('field'), lib:hslayout.m.route.param('lib')}};
+            return useConfig?
+                hslayout.m(hslayout.Config, Object.assign({source: myConfig2, context: [hslayout, header, left, main]}, param)) :
+                hslayout.m(docWindow, param);
+        }
     };
-    m.route(document.body, '/api', { 
+    hslayout.m.route(document.body, '/api', { 
         '/api':             Router,
         '/api/:lib':        Router,
         '/api/:lib/:field': Router
