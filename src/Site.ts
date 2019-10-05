25e8d3176663a85ef0a7b04b1ccea7b463330b79
hs <helpfulscripts@gmail.com>
2019-10-05 11:16:49 -0700

    moved page design into deisgn.json

--- src/Site.ts
+++ src/Site.ts
@@ -10,10 +10,13 @@ import * as main    from './view/MainDetail';
 
 export function init() {
     const Router = {
-        view: () => {
-            const param = {route: {field:hslayout.m.route.param('field'), lib:hslayout.m.route.param('lib')}};
-            return hslayout.m(hslayout.Config, Object.assign({source:'./design.json', context: [hslayout, header, left, main]}, param));
-        }
+        view: () => hslayout.m(hslayout.Config, 
+            Object.assign({source:'./design.json', context: [hslayout, header, left, main]}, 
+            {
+                route: {field:hslayout.m.route.param('field'), 
+                lib:hslayout.m.route.param('lib')}
+            }
+        ))
     };
     hslayout.m.route(document.body, '/api', { 
         '/api':             Router,
